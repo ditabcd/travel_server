256591371108e5398d1dce6eea3bc61db086bb3e
jelitasm <43837893+jelitasm@users.noreply.github.com>
2018-12-16 12:26:46 +0700

    tiket

--- application/controllers/Tiket.php
+++ application/controllers/Tiket.php
@@ -7,8 +7,12 @@ use Restserver\Libraries\REST_Controller;
 class tiket extends REST_Controller {
 //$this->response(array("status"=>"success","result" => $get_pembeli));
 //$this->response(array("status"=>"success"));
-	function tiket_get(){
-		$get_tiket = $this->db->query("SELECT * FROM tiket")->result();
+	function tiket_get($kata=''){
+		if ($kata!='') 
+		{
+			$this->db->or_like('kota',$kata);
+		}
+		$get_tiket = $this->db->get("tiket")->result();
 		$this->response(array("status"=>"success","result" => $get_tiket));
 	}
 }
